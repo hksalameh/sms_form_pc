ae6ea3377d3c5d357c6014928595daf347ed3f78
hksalameh <hksalameh@gmail.com>
2026-08-30 10:35:37 +0300

    Add SmsHks Android app build config

---
+++ android_app/app/build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.smshks.companion"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.smshks.companion"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
